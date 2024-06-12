24d0ff67d5aeaf0c359d29a83d03ead9db5aa4b1
carlosperales95 <carlos.perales95@opendeusto.es>
2024-06-12 20:02:25 +0200

    remove unusable stuff

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/carlosperales95/django-password-management-app.git  # Cloning project repository
-cd django-password-manager-app/ # Enter to project directory
+# git clone https://github.com/carlosperales95/django-password-management-app.git  # Cloning project repository
+# cd django-password-manager-app/ # Enter to project directory
 python3 -m venv pass_manage_venv # If not created, creating virtualenv
 source ./pass_manage_venv/bin/activate # Activating virtualenv
 pip3 install -r ./requirements.txt # Installing dependencies
